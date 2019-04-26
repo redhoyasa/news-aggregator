a1cfac6c38b5867b335ef324b2e41b3575785aea
redhoyasa <redhoyasa@gmail.com>
2019-04-26 16:43:00 +0700

    integrate with AWS Lambda

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby '~> 2.5.0'
 
 gem 'nokogiri'
-gem 'rake'
+gem 'rake', :group => :development
 gem 'telegram-bot-ruby'
 gem 'dotenv'
 
--- lambda.rb
+++ lambda.rb
@@ -1,10 +1,13 @@
-$LOAD_PATH = Dir['./vendor/bundle/ruby/2.5.0/gems/**/lib', './lib']
+load_paths = Dir['./lib']
+$LOAD_PATH.unshift(*load_paths)
+
 require 'open-uri'
 require 'nokogiri'
-require 'json'
 require 'news_aggregator'
+require 'json'
 
-def seanmctoday_telegram(event:, context:)
+
+def seanmctoday_telegram(event:, context:) 
   NewsAggregator.start
   { statusCode: 200, body: JSON.generate('Success to send news!') }
 end
